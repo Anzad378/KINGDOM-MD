900ef33af1dbe8215a06a0592e3d4c3277b0451e
ANZAD <anzadibayana@gmail.com>
2025-06-02 21:52:13 +0300

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -50,7 +50,7 @@ async (conn, mek, m, { from }) => {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363207624903731@newsletter',
+                    newsletterJid: '+120363401981451001@newsletter',
                     newsletterName: 'KINGDOM-MD 🤖',
                     serverMessageId: 143
                 }            
